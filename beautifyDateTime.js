3f60c61c1866e128fae8b580d8fedcc78b78f1fd
Zaber Al Rafian <36133897+rafian-git@users.noreply.github.com>
2018-08-13 13:54:59 +0600

    Update beautifyDateTime.js

--- beautifyDateTime.js
+++ beautifyDateTime.js
@@ -3,50 +3,50 @@ const beautifyDateTime = function(aDate){
   const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
-  var today =new Date(aDate);
-  var dd = today.getDate();
-  var mm = today.getMonth()+1; //January is 0!
-  var yyyy = today.getFullYear();
-  var hours=today.getHours() ;
-  var minutes=today.getMinutes() ;
+  var day =new Date(aDate);
+  var dd = day.getDate();
+  var mm = day.getMonth()+1; //January is 0!
+  var yyyy = day.getFullYear();
+  var hours=day.getHours() ;
+  var minutes=day.getMinutes() ;
   var ampm = hours >= 12 ? 'pm' : 'am';
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
   minutes = minutes < 10 ? '0'+minutes : minutes;
   var t = hours + ':' + minutes + ' ' + ampm;
-  var day = new Date();
+  var today = new Date();
 //  console.log(dd + "/" + mm +"/"+yyyy+"  "+hours+":"+minutes+" "+ampm);
 
-  if(dd == day.getDate() && mm == day.getMonth()+1 && yyyy== day.getFullYear()){
-    if (today.getHours() == day.getHours() && today.getMinutes() == day.getMinutes() ) {
+  if(dd == today.getDate() && mm == today.getMonth()+1 && yyyy== today.getFullYear()){
+    if (day.getHours() == today.getHours() && day.getMinutes() == today.getMinutes() ) {
       var nDate =  "Just now";
     }
-    else if (today.getHours() == day.getHours()) {
-      if(today.getMinutes() == day.getMinutes()-1 )
+    else if (day.getHours() == today.getHours()) {
+      if(day.getMinutes() == today.getMinutes()-1 )
       var nDate =  "A minute ago";
-      else if (today.getMinutes() == '59' && day.getMinutes()== "0")
+      else if (day.getMinutes() == '59' && today.getMinutes()== "0")
       var nDate =  "A minute ago";
-      else if (day.getMinutes() - today.getMinutes() >1){
-      var diff = day.getMinutes() - today.getMinutes();
+      else if (today.getMinutes() - day.getMinutes() >1){
+      var diff = today.getMinutes() - day.getMinutes();
       var nDate =  diff+" minutes ago";
       }
-      else if (today.getMinutes() >day.getMinutes()){
-      var diff = 60 - today.getMinutes();
-      diff += day.getMinutes();
+      else if (day.getMinutes() >today.getMinutes()){
+      var diff = 60 - day.getMinutes();
+      diff += today.getMinutes();
       var nDate =  diff+" minutes ago";
       }
-      else 
+      else
       var nDate =  "Within the hour";
-      
+
     }
-    else if (today.getHours() == day.getHours()-1) {
-      if (day.getMinutes() > today.getMinutes()) {
+    else if (day.getHours() == today.getHours()-1) {
+      if (today.getMinutes() > day.getMinutes()) {
         var nDate =  "An hour ago";
       }
     }
     else {
-      if (day.getHours() > today.getHours()) {
-        var diff = day.getHours() - today.getHours();
+      if (today.getHours() > day.getHours()) {
+        var diff = today.getHours() - day.getHours();
         var nDate =  diff+" hours ago";
       }
       else
@@ -55,12 +55,12 @@ const beautifyDateTime = function(aDate){
     return nDate;
   }
 // for yesterday
-  else if (dd == day.getDate()-1 && mm == day.getMonth()+1 && yyyy== day.getFullYear()) {
+  else if (dd == today.getDate()-1 && mm == today.getMonth()+1 && yyyy== today.getFullYear()) {
     var nDate = "yesterday at " + t;
 return nDate;
   }
 // for current year
-  else if (yyyy== day.getFullYear()) {
+  else if (yyyy== today.getFullYear()) {
     var nDate = monthNames[mm-1] + " " + dd + " at " + t;
 return nDate;
   }
